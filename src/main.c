36e41a69278312e7c17ecfa5001f52f12b94f498
Stheffanny N. Alves <stheffannyalvesnascimento@gmail.com>
2025-11-07 03:14:23 -0300

    Organiza a configuração dos pinos GPIO e adiciona a definição de FUNC_SSI para melhorar a legibilidade do código.

--- src/main.c
+++ src/main.c
@@ -13,7 +13,17 @@
 #define UART_DR *(uint32_t *) (UART0_BASE + 0x00) // Dados
 #define RESETS_RESET *(uint32_t *) (RESETS_BASE + 0x00)
 #define RESETS_RESET_DONE *(uint32_t *) (RESETS_BASE + 0x08) // pra zerar o bit
-#define GPIO6_CTRL *(uint32_t *) (IO_BANK0 + 0) // Configuração do pino Tx
+
+// Configuração dos GPIOS
+#define GPIO6_CTRL *(uint32_t *) (IO_BANK0 + 0x1C) 
+#define GPIO7_CTRL *(uint32_t *) (IO_BANK0 + 0x20) 
+#define GPIO8_CTRL *(uint32_t *) (IO_BANK0 + 0x24)
+#define GPIO9_CTRL *(uint32_t *) (IO_BANK0 + 0x28) 
+#define GPIO10_CTRL *(uint32_t *) (IO_BANK0 + 0x2C) 
+#define GPIO11_CTRL *(uint32_t *) (IO_BANK0 + 0x30) 
+
+
+
 #define SSI_CR0  *(uint32_t *) (SSIO_BASE + 0x00) // control register 0, define o formato(clock, modo SPI)
 #define SSI_CR1  *(uint32_t *) (SSIO_BASE + 0x04) // ctrl reg 1, habilita/desabilita o SSI para cada leitura
 #define SSI_DR0  *(uint32_t *) (SSIO_BASE + 0x08) // data reg, envio e recebimento de dados da FLASH
@@ -23,8 +33,8 @@
 #define TXFF_RST_BIT (1 << 5) // isola o bit 5(buffer/FIFO já cheio)
 #define UART0_RST_BIT (1 << 22) 
 #define FUNC_UART0 (1 << 0)
+#define FUNC_SSI 5
 #define SSI0_RST_BIT (1<< 16) // mascara pra sair do reset
-
 #define SSI_EN_BIT (1 << 0)
 
 
@@ -55,5 +65,17 @@ void ssi_init(void)
    SSI_CR1 &= ~SSI_EN_BIT; // SSI desabilitado
 
 
-   // configuração dos pinos
+   
+   GPIO6_CTRL = FUNC_SSI; // chip select(cs)
+   GPIO7_CTRL = FUNC_SSI; // Serial Clock (SCLK)
+
+   // quatros vias de dados, pra o chip ler quatros bits em um unico ciclo de clock.
+   GPIO8_CTRL = FUNC_SSI; // Serial Data 0 (SD0)
+   GPIO9_CTRL = FUNC_SSI; // Serial Data 1 (SD1)
+   GPIO10_CTRL = FUNC_SSI; // Serial Data 2 (SD2)
+   GPIO11_CTRL = FUNC_SSI; // Serial Data 3 (SD3)
+
+
+
+   
 }
