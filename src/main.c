9594f7c8872346f8ab010622c254a3f46f075910
Stheffanny N. Alves <stheffannyalvesnascimento@gmail.com>
2025-11-09 21:06:57 -0300

    Refatora o código para rodar na SRAM, ajusta definições de registradores para uso correto e melhora a função de aquisição da FLASH.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-// codigo em desenvolvimento, retomada pós provas.
+// codigo a ser refatorado, o codigo vai rodar na SRAM invés da Flash.
 
 
 #include <stdint.h>
@@ -12,13 +12,13 @@
 #define XIP_SSI_BASE (0x18000000)
 
 // mapeamento dos registradores  de reset
-#define RESETS_RESET *(uint32_t *) (RESETS_BASE + 0x00)
-#define RESETS_RESET_DONE *(uint32_t *) (RESETS_BASE + 0x08) // pra zerar o bit
+#define RESETS_RESET *(volatile uint32_t *) (RESETS_BASE + 0x00)
+#define RESETS_RESET_DONE *(volatile uint32_t *) (RESETS_BASE + 0x08) // pra zerar o bit
 
 
 // regs de UART
-#define UART_FR *(uint32_t *) (UART0_BASE + 0x18) // Status
-#define UART_DR *(uint32_t *) (UART0_BASE + 0x00) // Dados
+#define UART_FR *(volatile uint32_t *) (UART0_BASE + 0x18) // Status
+#define UART_DR *(volatile uint32_t *) (UART0_BASE + 0x00) // Dados
 
 // mascaras UART
 
@@ -30,22 +30,22 @@
 
 
 
-#define GPIO0_CTRL *(uint32_t *) (IO_BANK0 + 0x004) // Tx
-#define GPIO1_CTRL *(uint32_t *) (IO_BANK0 + 0x00c) // Rx
+#define GPIO0_CTRL *(volatile uint32_t *) (IO_BANK0 + 0x004) // Tx
+#define GPIO1_CTRL *(volatile uint32_t *) (IO_BANK0 + 0x00c) // Rx
 #define FUNC_UART0 2
 
 
 
-#define CTRL_R0  *(uint32_t *) (XIP_SSI_BASE + 0x00) // control register 0, define o formato(clock, modo SPI)
-#define SSI_ENR  *(uint32_t *) (XIP_SSI_BASE + 0x08) // ctrl reg 1, habilita/desabilita o SSI para cada leitura
-#define DR0  *(uint32_t *) (XIP_SSI_BASE + 0x60) // data reg, envio e recebimento de dados da FLASH
-#define SR  *(uint32_t *)  (XIP_SSI_BASE + 0X28)
+#define CTRL_R0  *(volatile uint32_t *) (XIP_SSI_BASE + 0x00) // control register 0, define o formato(clock, modo SPI)
+#define SSI_ENR  *(volatile uint32_t *) (XIP_SSI_BASE + 0x08) // ctrl reg 1, habilita/desabilita o SSI para cada leitura
+#define DR0  *(volatile uint32_t *) (XIP_SSI_BASE + 0x60) // data reg, envio e recebimento de dados da FLASH
+#define SR  *(volatile uint32_t *)  (XIP_SSI_BASE + 0X28)
 
 // Valores escritos nos registradores, mascaras
 #define TXFF_RST_BIT (1 << 5) // isola o bit 5(buffer/FIFO já cheio)
 #define UART0_RST_BIT (1 << 22) 
 #define SSI_EN_BIT (1 << 0)
-#define SR_TFNF (1 << 2) // transmit fifo not full, diz quando a FIFO de TX  *não* tá cheio
+#define SR_TFNF (1 << 1) // transmit fifo not full, diz quando a FIFO de TX  *não* tá cheio(verificar)
 #define SR_RFNE (1 << 3) // Receive FIFO not empty, diz quando o dado chegou
 
 
@@ -62,7 +62,7 @@ void uart_putc(char data) // funcao de envio de bits
 }
    
 
-// funcao de inicialização do SSI
+// funcao de inicialização do SSI(correção critica)
 void ssi_init(void)
 {
    // 1. Desabilita o XIP
@@ -79,61 +79,52 @@ void ssi_init(void)
    
 }
 
+
+
+
+// func pra falar com o SPI
+static uint8_t ssi_transfer_byte(uint8_t data_out)
+{
+   while (!(SR & SR_TFNF)) // espera ter espaço
+   {
+      /* code */
+   }
+   DR0 = data_out; 
+   
+   while (!(SR & SR_TFNF)) // espera o byte de resposta chegar
+   {
+      /* code */
+   }
+
+   return (uint8_t)DR0;
+}
+
+
+
+
+
+
 // funcao que faz a aquisição da FLASH
 uint8_t ssi_read_byte(uint32_t address)
 {
-   // Envio do comando Fast Read
-   while ( !(SR & SR_TFNF) )
-   {
-      // espera TX
-   }
-
-   DR0 = 0x0B; // saiu, tem espaço
+   // 1. Envio do comando Fast Read
+   ssi_transfer_byte(0x0B);
    
-   // Envia os bits 23-16(byte 1)
-   while ( !(SR & SR_TFNF))
-   {
-      
-
-   }
-   DR0 = (address >> 16) & 0xFF; 
+   // 2. Envia os bits 23-16(byte 1)
+   ssi_transfer_byte((address >> 16) & 0xFF); 
    
    // 3. Envia os bits 15-8(byte 2)
-   while ( !(SR & SR_TFNF))
-   {
-      
-   }
-   DR0 = (address >> 8) & 0xFF; 
+   ssi_transfer_byte((address >> 8) & 0xFF); 
    
    
    // 4. Envia os bits 7-0(byte 3)
-   while ( !(SR & SR_TFNF))
-   {
+   ssi_transfer_byte(address & 0xFF); 
 
-   }
-   DR0 = address & 0xFF; 
-   
-   
-   
    // 5. envia um "dummy byte"(byte ficticio)
-   while ( !(SR & SR_TFNF))
-   {
-      // espera TX
-   }
+   ssi_transfer_byte(0x15);
 
-   DR0 = 0x15;
-
-   // 6. Espera o dado de resposta chegar
-
-   while ( !(SR & SR_RFNE))
-   {
-      // espera RX
-   }   
-   
-   // 7. Lê o dado
-   uint8_t dado = DR0; // A FIFO me fornece um byte
-   
-   return dado;
+   // 6. Envia um byte nulo(pra gerar clock)
+   return ssi_transfer_byte(0x00);
    
 }
 
@@ -155,7 +146,7 @@ void uart_init(void)
    GPIO1_CTRL = FUNC_UART0;
    
 
-   // baud rate e control register(incompleto)
+   // baud rate = clk do sistema/(16 * baud rate)
 }
 
 
